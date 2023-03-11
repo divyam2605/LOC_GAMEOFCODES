1f5377775b2af50de137328ed9741dbd1e37a39a
mahekupadhye31 <114089022+mahekupadhye31@users.noreply.github.com>
2023-03-12 01:36:35 +0530

    Add files via upload

---
+++ src/pages/Candidate.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Chat from "../components/Chat";
+
+const Candidate = () => {
+  return (
+    <div>
+        <Nav/>
+        <Chat/>
+        h1
+      
+    </div>
+  )
+}
+
+export default Candidate
---
+++ src/pages/Recruiter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav1 from '../components/Nav1'
+import Chat from "../components/Chat";
+
+const Recruiter = () => {
+  return (
+    <div>
+      <Nav1/>
+      <Chat/>
+      recruiter
+    </div>
+  )
+}
+
+export default Recruiter
